09dc2a8755b3cce71c70781aeaec06ad2f1ae229
zaxdm <esteban.dioses@tecsup.edu.pe>
2025-06-23 00:00:58 -0500

    Update config.php

--- Core/config.php
+++ Core/config.php
@@ -1,19 +1,21 @@
 <?php  
-	// Datos de conexión a base de datos en InfinityFree
+    // Datos de conexión a base de datos en InfinityFree
     const DB_HOST = "sql206.infinityfree.com";
     const DB_NAME = "if0_39291681_botica";
     const DB_USER = "if0_39291681";
     const DB_PASSWORD = "GXzrIt5MtEG9";
     const SGBD = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+
     // Datos para encriptación (estos sí pueden ser constantes porque son valores fijos)
     const METHOD = "AES-256-CBC";
     const SECRET_KEY = '$#*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890#*';
     const SECRET_IV = '20001109108103975194753';
 
     try {
-        $conexion = new PDO($SGBD, $DB_USER, $DB_PASSWORD);
+        // Ojo: aquí no se usa el signo $
+        $conexion = new PDO(SGBD, DB_USER, DB_PASSWORD);
         $conexion->exec("SET NAMES 'UTF8'");
-        // echo "✅ Conexión exitosa a PostgreSQL";
+        // echo "✅ Conexión exitosa a MySQL en InfinityFree";
     } catch (Exception $e) {
         die("❌ Error de conexión: " . $e->getMessage());
     }
